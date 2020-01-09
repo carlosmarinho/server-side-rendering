dc2dd795befd81ec46ad5b688347eaab18d8f412
Carlos Luiz Marinho da Silva Junior <carluizfla@hotmail.com>
2020-01-09 01:02:23 -0300

    adding redux and thats definitely work

---
+++ src/client/components/UserList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UserList extends Component {
+    componentDidMount() {
+        this.props.fetchUsers();
+    }
+
+    renderUsers() {
+        return this.props.users.map(user => {
+            return <li key={user.id}>{user.name}</li>
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                here's a big list of usrs:
+                <ul>
+                    {this.renderUsers()}
+                </ul>
+            </div>
+            
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        users: state.users
+    }
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UserList);
